8ec9d9947cc5549fd26efea0f9ac948d5714c826
Contreras71399 <145361635+Contreras71399@users.noreply.github.com>
2023-10-13 23:32:59 -0600

    Create BatallaPokemon.java

---
+++ Pokemon_Abstraccion/Pokemon/BatallaPokemon.java
@@ -0,0 +1,24 @@
+package ListaMovimiento;
+
+public class BatallaPokemon {
+    public static void main(String[]args){
+       PokemonTipo Pokemon1=new PokemonTipo("Gyarados",10,"Volador");
+       PokemonTipo Pokemon2=new PokemonTipo("Jolteon",10,"Electrico");
+       
+       realizarAtaque(Pokemon1,Pokemon2,"Hidrobomba");
+       realizarAtaque(Pokemon2,Pokemon1,"Impactrueno");
+       
+       System.out.println(Pokemon1.getNombre()+"tiene ahora"+Pokemon1.getHP()+"tiene puntos de vida ");
+    }
+
+    public static void realizarAtaque(Pokemon Ata, Pokemon obj, String ataque) {
+        int  danio= dano(ataque);
+        int PuntosVida=objetivo.getHP()
+            PuntosVida=danio;
+        objetivo.setHP(PuntosVida);
+        System.out.println(Ata.getNombre()+ "ataca a "+ obj.getNombre()+ "con" + ataque+ "y causa" + dano + "Puntos de daño");
+                public static int dano (String ataque){
+                    return 1;
+    }
+                }
+                }
